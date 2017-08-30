8cf089c668abf79258da71dd42c81785f610713a
Chris <chris@usablehq.com>
2017-08-30 21:19:39 +0100

    Layout and event improvements

--- src/App.js
+++ src/App.js
@@ -1,48 +1,66 @@
 import React, { Component } from 'react';
-import record from './img/record.png';
 import './App.css';
 
 import { Howl } from 'howler';
 import YouTube from 'react-youtube';
 
+import Record from './Record.js';
+
 class App extends Component {
+
     constructor() {
+
         super();
 
         this.crackle = new Howl({ src: ['./crackle.ogg'], loop: true, autoplay: false, autoload: true});
         this.state = {
             playing: false,
             playlistPos: 0,
-            playlist: ['8HlTCn4s6dg','eq95TdCaXSU','3BU8hVTg9Xg','VUvj94b88rA'],
-            videoId: '8HlTCn4s6dg'
+            playlist: ['nt-C3eMo5WA','3eYSUxoRc0U','_7qhdcaX8Q0','WEi9ZQrEjr8','qLrnkK2YEcE','MV_3Dpw-BRY','rDBbaGCCIhk'],
+            videoId: 'nt-C3eMo5WA',
+            autoplay: 0
         }
-        this.onReady      = this.onReady.bind(this);
+
+        this.onReady       = this.onReady.bind(this);
         this.onChangeVideo = this.onChangeVideo.bind(this);
-        this.onPlayVideo  = this.onPlayVideo.bind(this);
-        this.onPauseVideo = this.onPauseVideo.bind(this);
-        this.onStopVideo  = this.onStopVideo.bind(this);
+        this.onPlay        = this.onPlay.bind(this);
+        this.onEnd         = this.onEnd.bind(this);
+        this.onPlayVideo   = this.onPlayVideo.bind(this);
+        this.onPauseVideo  = this.onPauseVideo.bind(this);
+        this.onStopVideo   = this.onStopVideo.bind(this);
 
     }
 
     onReady(event) {
-        event.target.setVolume(30);
+        event.target.setVolume(100);
         this.setState({
             player: event.target,
+            vData: event.target.getVideoData()
         });
     }
 
     onChangeVideo() {
+        if (!this.crackle.playing()) { this.crackle.play() };
         var p = this.state.playlistPos;
         if (p===this.state.playlist.length-1) { p = 0; } else { p++; }
         this.setState({
             playlistPos: p,
-            videoId: this.state.playlist[p]
+            videoId: this.state.playlist[p],
+            playing: true,
+            autoplay: 1
         });
+    }
+
+    onPlay(event){
+        this.setState({ vData: this.state.player.getVideoData(), playing: true })
+    }
+
+    onEnd(event){
+        this.onChangeVideo();
     }
 
     onPlayVideo() {
         this.crackle.play();
-        this.setState({ playing: true });
         this.state.player.playVideo();
     }
 
@@ -59,27 +77,29 @@ class App extends Component {
     render() {
 
         const opts = {
-            height: '390',
-            width: '640',
+            height: '0',
+            width: '0',
             playerVars: {
                 controls: 0,
-                autoplay: 0
+                autoplay: this.state.autoplay
             }
         }
 
         return (
             <div className="App">
-                <h1>Crackly Record</h1>
+                <h1>YT1200</h1>
                 <YouTube
-                    videoId={this.state.videoId}
-                    opts={opts}
-                    onReady={this.onReady}
+                    videoId={ this.state.videoId }
+                    opts={ opts }
+                    onReady={ this.onReady }
+                    onPlay={ this.onPlay }
+                    onEnd={ this.onEnd }
                 /><br/>
-                <button onClick={this.onPlayVideo}>Play</button>
-                <button onClick={this.onPauseVideo}>Pause</button>
-                <button onClick={this.onStopVideo}>Stop</button>
-                <button onClick={this.onChangeVideo}>Next</button>
-                <img src={ record } alt="Record" className={'record '+(this.state.playing===true?'spinning':'')} />
+                <div className='button' onClick={this.onPlayVideo}>Play</div>
+                <div className='button' onClick={this.onPauseVideo}>Pause</div>
+                <div className='button' onClick={this.onStopVideo}>Stop</div>
+                <div className='button' onClick={this.onChangeVideo}>Next</div>
+                <Record vData={ this.state.vData } playing={ this.state.playing } />
             </div>
         );
     }
---
+++ src/Record.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import record from './img/record.png';
+import './Record.css';
+
+class Record extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { title: 'Unknown', label: '' }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            label: 'https://img.youtube.com/vi/'+nextProps.vData.video_id+'/0.jpg',
+            title: nextProps.vData.title
+        })
+    }
+
+    render() {
+        return (
+            <div className={'Record '+(this.props.playing===true?'spinning':'')} style={{ backgroundImage: 'url('+record+')' }} >
+                <div className="label">
+                    <img src={ this.state.label } alt='title' />
+                    <span className="title">{ this.state.title }</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Record;
