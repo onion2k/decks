338e949fe4bd2ab1ee4fd3aa2c86dce7260d3c8c
Chris Neale <chris@komododigital.co.uk>
2017-08-30 14:02:50 +0100

    Basic playlist

--- src/App.js
+++ src/App.js
@@ -10,11 +10,17 @@ class App extends Component {
         super();
 
         this.crackle = new Howl({ src: ['./crackle.ogg'], loop: true, autoplay: false, autoload: true});
-        this.state = { playing: false }
-        this.onReady = this.onReady.bind(this);
-        this.onPlayVideo = this.onPlayVideo.bind(this);
+        this.state = {
+            playing: false,
+            playlistPos: 0,
+            playlist: ['8HlTCn4s6dg','eq95TdCaXSU','3BU8hVTg9Xg','VUvj94b88rA'],
+            videoId: '8HlTCn4s6dg'
+        }
+        this.onReady      = this.onReady.bind(this);
+        this.onChangeVideo = this.onChangeVideo.bind(this);
+        this.onPlayVideo  = this.onPlayVideo.bind(this);
         this.onPauseVideo = this.onPauseVideo.bind(this);
-        this.onStopVideo = this.onStopVideo.bind(this);
+        this.onStopVideo  = this.onStopVideo.bind(this);
 
     }
 
@@ -22,6 +28,15 @@ class App extends Component {
         event.target.setVolume(30);
         this.setState({
             player: event.target,
+        });
+    }
+
+    onChangeVideo() {
+        var p = this.state.playlistPos;
+        if (p===this.state.playlist.length-1) { p = 0; } else { p++; }
+        this.setState({
+            playlistPos: p,
+            videoId: this.state.playlist[p]
         });
     }
 
@@ -44,8 +59,8 @@ class App extends Component {
     render() {
 
         const opts = {
-            height: '39',
-            width: '64',
+            height: '390',
+            width: '640',
             playerVars: {
                 controls: 0,
                 autoplay: 0
@@ -56,13 +71,14 @@ class App extends Component {
             <div className="App">
                 <h1>Crackly Record</h1>
                 <YouTube
-                    videoId="8HlTCn4s6dg"
+                    videoId={this.state.videoId}
                     opts={opts}
                     onReady={this.onReady}
-                />
+                /><br/>
                 <button onClick={this.onPlayVideo}>Play</button>
                 <button onClick={this.onPauseVideo}>Pause</button>
                 <button onClick={this.onStopVideo}>Stop</button>
+                <button onClick={this.onChangeVideo}>Next</button>
                 <img src={ record } alt="Record" className={'record '+(this.state.playing===true?'spinning':'')} />
             </div>
         );
