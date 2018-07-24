b3e14289482f1f86af354b7379daa8c6bf4074bf
Chris Neale <chris@komododigital.co.uk>
2018-07-24 08:49:19 +0100

    Added knobs to stories

--- src/Components/Controls/Controls.stories.js
+++ src/Components/Controls/Controls.stories.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 
 import StoryRouter from 'storybook-react-router';
 import { linkTo } from '@storybook/addon-links';
 
 import Controls from './index.js';
 
+const onPlayVideo = action('play video');
+const onPauseVideo = action('pause video');
+const onStopVideo = action('stop video');
+const onChangeVideo = action('change video');
+const onToggle = action('toggle');
+
 storiesOf('Controls', module)
+  .addDecorator(
+    withKnobs
+  )
   .addDecorator(
     StoryRouter(
       {
-        '/': linkTo('Linked stories', 'home'),
-        '/articles/*': linkTo('Linked stories', 'article'),
+        '/playlists': linkTo('playlists', 'Basic'),
       },
-      { initialEntries: ['/about'] }
+      { initialEntries: ['/playlists'] }
     )
   )
-  .add('about', () => <Controls />);
+  .add('about', () => {
+
+    // const shuffle = boolean("Shuffle", false, "toggle");
+
+    return <Controls 
+      onStopVideo={onStopVideo} 
+      onPlayVideo={onPlayVideo} 
+      onPauseVideo={onPauseVideo} 
+      onChangeVideo={onChangeVideo} 
+      onToggle={onToggle} 
+      repeat={boolean("Repeat", false, "toggle")}
+      shuffle={boolean("Shuffle", false, "toggle")}
+      crackle={boolean("Crackle", false, "toggle")}
+      video={boolean("Video", false, "toggle")}
+      />
+  });
--- src/Components/PlaylistManager/PlaylistManager.stories.js
+++ src/Components/PlaylistManager/PlaylistManager.stories.js
@@ -3,7 +3,20 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import PlaylistManager from './index.js';
 
+import StoryRouter from 'storybook-react-router';
+import { linkTo } from '@storybook/addon-links';
+
+const playlists = [];
+const addList = action('add-list')
+
 storiesOf('PlaylistManager', module)
-  .add('Basic', () => (
-    <PlaylistManager />
-  ));
+  .addDecorator(
+    StoryRouter(
+      {
+        '/': linkTo('Linked stories', 'home'),
+        '/articles/*': linkTo('Linked stories', 'article'),
+      },
+      { initialEntries: ['/about'] }
+    )
+  )
+  .add('Basic', () => <PlaylistManager playlists={playlists} addList={addList} />);
