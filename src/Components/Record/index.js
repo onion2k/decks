4f2b2590e9d9ca3b2fae6b3b26f866b14d48b8a5
Chris Neale <chris@komododigital.co.uk>
2018-08-16 08:15:02 +0100

    Tone arm drag functions added back

--- src/Components/Record/index.js
+++ src/Components/Record/index.js
@@ -19,22 +19,20 @@ class Record extends Component {
   constructor(props) {
     super(props);
 
-    // this.armMove = this.armMove.bind(this);
-    // this.armUp = this.armUp.bind(this);
+    this.armMove = this.armMove.bind(this);
+    this.armUp = this.armUp.bind(this);
     // this.tick = this.tick.bind(this);
     this.lastX = 0;
     this.requestAnimationFrameId = null;
 
-    // this.state = {
-    //   requestAnimationFrameId: null,
-    //   title: "Unknown",
-    //   label: "",
-    //   dragtone: false,
-    //   tonestart: -54,
-    //   toneangle: -54,
-    //   tonestyle: "rotate(-54deg)",
-    //   millis: Date.now()
-    // };
+    this.state = {
+      requestAnimationFrameId: null,
+      dragtone: false,
+      tonestart: -54,
+      toneangle: -54,
+      tonestyle: "rotate(-54deg)",
+      millis: Date.now()
+    };
   }
 
   track = autorun(() => {
@@ -88,79 +86,79 @@ class Record extends Component {
     // });
   //  }
 
-  // armMove(e) {
-  //   if (this.state.dragtone) {
-  //     if (this.state.requestAnimationFrameId) {
-  //       cancelAnimationFrame(this.state.requestAnimationFrameId);
-  //     }
-  //     let toneangle = this.state.tonestart + (e.pageX - this.lastX) / 10;
-  //     if (toneangle < -54) {
-  //       toneangle = -54;
-  //     }
-  //     if (toneangle > -19) {
-  //       toneangle = -19;
-  //     }
-  //     this.setState({
-  //       requestAnimationFrameId: false,
-  //       toneangle: toneangle,
-  //       tonestyle: "rotate(" + toneangle + "deg)"
-  //     });
-  //   }
-  // }
+  armMove(e) {
+    if (this.state.dragtone) {
+      if (this.state.requestAnimationFrameId) {
+        cancelAnimationFrame(this.state.requestAnimationFrameId);
+      }
+      let toneangle = this.state.tonestart + (e.pageX - this.lastX) / 10;
+      if (toneangle < -54) {
+        toneangle = -54;
+      }
+      if (toneangle > -19) {
+        toneangle = -19;
+      }
+      this.setState({
+        requestAnimationFrameId: false,
+        toneangle: toneangle,
+        tonestyle: "rotate(" + toneangle + "deg)"
+      });
+    }
+  }
 
-  // armUp(e) {
-  //   var toneangle, per;
+  armUp(e) {
+    var toneangle, per;
 
-  //   let perCal = function(s, e, p) {
-  //     let tl = Math.abs(s) - Math.abs(e);
-  //     let per = p + Math.abs(s);
-  //     return (per / tl) * 100;
-  //   };
+    let percentageTrackDistance = function(s, e, p) {
+      let tl = Math.abs(s) - Math.abs(e);
+      let per = p + Math.abs(s);
+      return (per / tl) * 100;
+    };
 
-  //   this.setState({ dragtone: false, tonestart: this.state.toneangle });
+    this.setState({ dragtone: false, tonestart: this.state.toneangle });
 
-  //   toneangle = this.state.toneangle;
+    toneangle = this.state.toneangle;
 
-  //   if (toneangle > -20) {
-  //     //off
-  //   } else if (toneangle > -25.6) {
-  //     per = perCal(-25.6, -20, toneangle);
-  //     this.props.onTrack(5, per);
-  //   } else if (toneangle > -28.5) {
-  //     per = perCal(-28.5, -25.6, toneangle);
-  //     this.props.onTrack(4, per);
-  //   } else if (toneangle > -34.2) {
-  //     per = perCal(-34.2, -28.5, toneangle);
-  //     this.props.onTrack(3, per);
-  //   } else if (toneangle > -39.7) {
-  //     per = perCal(-39.7, -34.2, toneangle);
-  //     this.props.onTrack(2, per);
-  //   } else if (toneangle > -43.4) {
-  //     per = perCal(-43.4, -39.7, toneangle);
-  //     this.props.onTrack(1, per);
-  //   } else if (toneangle > -47.6) {
-  //     per = perCal(-47.6, -43.4, toneangle);
-  //     this.props.onTrack(0, per);
-  //   } else {
-  //     //off
-  //   }
-  // }
+    if (toneangle > -20) {
+      //off
+    } else if (toneangle > -25.6) {
+      per = percentageTrackDistance(-25.6, -20, toneangle);
+      // this.props.onTrack(5, per);
+    } else if (toneangle > -28.5) {
+      per = percentageTrackDistance(-28.5, -25.6, toneangle);
+      // this.props.onTrack(4, per);
+    } else if (toneangle > -34.2) {
+      per = percentageTrackDistance(-34.2, -28.5, toneangle);
+      // this.props.onTrack(3, per);
+    } else if (toneangle > -39.7) {
+      per = percentageTrackDistance(-39.7, -34.2, toneangle);
+      // this.props.onTrack(2, per);
+    } else if (toneangle > -43.4) {
+      per = percentageTrackDistance(-43.4, -39.7, toneangle);
+      // this.props.onTrack(1, per);
+    } else if (toneangle > -47.6) {
+      per = percentageTrackDistance(-47.6, -43.4, toneangle);
+      // this.props.onTrack(0, per);
+    } else {
+      //off
+    }
+  }
 
   render() {
 
     return (
       <div
         className="Deck"
-        // onMouseDown={e => {
-        //   this.lastX = e.pageX;
-        //   this.setState({ dragtone: true });
-        // }}
-        // onMouseUp={e => {
-        //   this.armUp(e);
-        // }}
-        // onMouseMove={e => {
-        //   this.armMove(e);
-        // }}
+        onMouseDown={e => {
+          this.lastX = e.pageX;
+          this.setState({ dragtone: true });
+        }}
+        onMouseUp={e => {
+          this.armUp(e);
+        }}
+        onMouseMove={e => {
+          this.armMove(e);
+        }}
       >
         <div
           className={
@@ -183,7 +181,7 @@ class Record extends Component {
             draggable="false"
             src={tonearm}
             alt="Tonearm"
-            style={{ transform: "" }}
+            style={{ transform: this.state.tonestyle }}
           />
         </div>
       </div>
