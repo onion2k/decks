2e64e0998aff7a55c588c0585eb9743f10654c10
Chris Neale <chris@komododigital.co.uk>
2018-08-24 08:49:09 +0100

    Todo for next progression based on settings

--- src/state/yt1210PlaylistManager.js
+++ src/state/yt1210PlaylistManager.js
@@ -106,6 +106,8 @@ export default class yt1210PlaylistManager {
     };
 
     getNextTrack = () => {
+        // if shuffle
+        // if repeat
         return this.tracks[++this.track];
     };
 
